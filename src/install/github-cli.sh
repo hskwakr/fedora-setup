032e4a1026937010a8f1412600892c6f0aed2e00
hskwakr <33633391+hskwakr@users.noreply.github.com>
2025-01-27 00:36:25 +0900

    Fix

--- src/install/github-cli.sh
+++ src/install/github-cli.sh
@@ -14,7 +14,7 @@ fi
 # ------------------------------------------------------------------------------
 # DNF5 installation commands
 sudo dnf install dnf5-plugins
-sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf config-manager addrepo --overwrite --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
 sudo dnf -y install gh --repo gh-cli
 
 # ------------------------------------------------------------------------------
